9f0db97a84a1b496bc286b566f59db462ab69743
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-01 15:47:17 +0530

    Commit

---
+++ PrimeNumberorNot/PrimeNumber/PrimeNum.cs
@@ -0,0 +1,20 @@
+﻿namespace PrimeNumber
+{
+    public class PrimeNum
+    {
+        public static Boolean Prime(int a)
+        {
+            Boolean result = false;
+            for (int i = 2; i < a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ PrimeNumberorNot/PrimeNumberorNot/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrimeNumber;
+Console.WriteLine("Enter Number");
+int Number = Convert.ToInt32(Console.ReadLine());
+Boolean result = PrimeNum.Prime(Number);
+if (!result)
+{
+   Console.WriteLine("Given Number is Prime");
+}
+else
+{ 
+    Console.WriteLine("Given Number is Not Prime");
+}
