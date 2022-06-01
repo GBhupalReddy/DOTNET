c3ba0379c5c83bee6e5ee290a9bb771ab5051c28
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-01 15:32:34 +0530

    Commit

---
+++ BiggNumberorSmallNumber/BigNumber/BigNum.cs
@@ -0,0 +1,21 @@
+﻿namespace BigNumber
+{
+    public class BigNum
+    {
+        public static int big(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if ((firstNumber > secondNumber) && (firstNumber > thirdNumber))
+            {
+                return firstNumber;
+            }
+            else if ((secondNumber > firstNumber) && (secondNumber > thirdNumber))
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return thirdNumber;
+            }
+        }
+    }
+}
---
+++ BiggNumberorSmallNumber/BiggNumberorSmallNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more inform
+using SmallNumber;
+using BigNumber;
+Console.WriteLine("Enter firstNumber");
+int firstNumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter secondNumber");
+int secondNumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter thirdNumber");
+int thirdNumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter do u want SmallNumber or BigNumber (small/big)");
+string Operation=Console.ReadLine();
+switch (Operation)
+{
+    case "small":
+        Console.WriteLine("Small Number is" + SmallNum.Small(firstNumber, secondNumber, thirdNumber)) ;
+        break;
+
+    case "big":
+        Console.WriteLine("Big Number is :  " + BigNum.big(firstNumber, secondNumber, thirdNumber));
+        break;
+   default:
+        Console.WriteLine("It Diplay only Small/Big NUmbrer");
+        break;
+}
+
