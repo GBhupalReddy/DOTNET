e0c1e42c3138fd8ae55bc45a935694c0b3625cf1
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-01 14:42:12 +0530

    Commit

---
+++ Calculation/ArityhmeticOperations/ArityhmeticOperation.cs
@@ -0,0 +1,48 @@
+﻿namespace ArityhmeticOperations
+{
+    public class ArityhmeticOperation
+    {
+        private static int secondNumber;
+
+        public static int MathOperations(int firstNumber, char Operator, int secondNumber)
+        {
+            switch (Operator)
+            {
+                case '+':
+                    return Sum(firstNumber, secondNumber);
+                case '-':
+                    return Subtraction(firstNumber, secondNumber);
+                case '*':
+                    return Multiplication(firstNumber, secondNumber);
+                case '/':
+                    return Division(firstNumber, secondNumber);
+                case '%':
+                    return Modulus(firstNumber, secondNumber);
+                default:
+                    throw new Exception("Enter arithmetic operator only");
+            }
+        }
+        public static int Sum(int firstNumber, int secondNumber)
+        {
+            return (firstNumber + secondNumber);
+        }
+        public static int Subtraction(int firstNumber, int secondNumber)
+        {
+            return (firstNumber - secondNumber);
+        }
+        public static int Multiplication(int firstNumber, int secondNumberb)
+        {
+            return (firstNumber * secondNumber);
+        }
+        public static int Division(int firstNumber, int secondNumber)
+        {
+            return (firstNumber / secondNumber);
+        }
+        public static int Modulus(int firstNumber, int secondNumber)
+        {
+            return (firstNumber % secondNumber);
+        }
+
+
+    }
+}
---
+++ Calculation/Calculation/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArityhmeticOperations;
+Console.WriteLine("Enter firstNumber");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter arithmetic operator");
+char Operator = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Enter secondNumber");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+try
+{
+    Console.WriteLine("Result: " + ArityhmeticOperation.MathOperations(firstNumber, Operator, secondNumber));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message.ToString());
+}
