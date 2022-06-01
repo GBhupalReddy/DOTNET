da5a0cc5b9e909d7b326d0b651935f352677458f
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-01 16:05:41 +0530

    Commit

---
+++ EvenorOdd/EvenOdd/OddEven.cs
@@ -0,0 +1,18 @@
+﻿namespace EvenOdd
+{
+    public class OddEven
+    {
+      public static void OddorEven(int Number)
+      {
+        if (Number % 2 == 0)
+        {
+            Console.WriteLine("Given Number id Even");
+        }
+        else
+        {
+            Console.WriteLine("Given Number Odd");
+        }
+      }
+    }
+
+}
---
+++ EvenorOdd/EvenorOdd/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EvenOdd;
+
+Console.WriteLine("Enter Number");
+int Number = Convert.ToInt32(Console.ReadLine());
+OddEven.OddorEven(Number);
