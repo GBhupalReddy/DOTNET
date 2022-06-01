9b84cecbee344aa6486c1c99fe0e216613f1b589
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-01 16:17:31 +0530

    Commit

---
+++ NumberofDigits/No.ofDigits/Digits.cs
@@ -0,0 +1,17 @@
+﻿namespace No.ofDigits
+{
+    public class Digits
+    {
+        public static int Count(int Number)
+        {
+            int Result = 0;
+            while (Number != 0)
+            {
+                Number = Number / 10;
+                Result++;
+            }
+            return Result;
+
+        }
+    }
+}
---
+++ NumberofDigits/NumberofDigits/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using No.ofDigits;
+
+Console.WriteLine("Enter the number");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number of digits :" + Digits.Count(Number));
