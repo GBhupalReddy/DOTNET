96bf96643d1fb7670633a3f299f2ff4c50b7cb43
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-02 18:10:42 +0530

    Commit 02 june

---
+++ PrintArrayElements/PrintArrayElements/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReadArrayElements;
+Console.WriteLine("Enter Array Length");
+int ArrayLength=Convert.ToInt32(Console.ReadLine());
+int [] Array = ReadArray.Read(ArrayLength);
+for(int i = 0; i < ArrayLength; i++)
+{
+    Console.WriteLine(Array[i]);
+}
---
+++ PrintArrayElements/ReadArrayElements/ReadArray.cs
@@ -0,0 +1,16 @@
+﻿namespace ReadArrayElements
+{
+    public class ReadArray
+    {
+        public static int[] Read(int Number)
+        { 
+            int [] array = new int[Number];
+            for(int i = 0; i < Number; i++)
+            {
+                Console.WriteLine($"Enter {i} index Number : ");
+                array[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            return array;
+        }
+    }
+}
