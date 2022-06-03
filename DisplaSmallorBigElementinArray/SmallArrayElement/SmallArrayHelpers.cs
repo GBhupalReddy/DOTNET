3ebd6a307b1006072b5067ae8edd16003d3fde93
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-03 15:22:42 +0530

    Commit 03 june

---
+++ DisplaSmallorBigElementinArray/SmallArrayElement/SmallArrayHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace SmallArrayElement
+{
+    internal static class SmallArrayHelpers
+    {
+        public static int SmallElement(int Number, int[] Array2)
+        {
+            Array.Sort(Array2);
+            int Result=0;
+            for (int i = 0; i < Array2.Length; i++)
+            {
+                if (i == Number - 1)
+                {
+                    return Result = Array2[i];
+                }
+            }
+            return Result;
+        }
+    }
+}
