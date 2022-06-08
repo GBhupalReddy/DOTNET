80cad5a08fe182c4e050e2cf560485c8c4b19dbe
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-08 18:30:50 +0530

    Commit 08 june

---
+++ june 08/Out-Parameter/Out-Parameter/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 12, j = 30, sum = 0, prod = 0;
+Console.WriteLine($"Before Using Out Parameter i = {i} j = {j} sum = {sum} prod = {prod}");
+SumProd(i, j, out sum, out prod);
+Console.WriteLine($"After Using Out Parameter i = {i} j = {j} sum = {sum} prod = {prod} ");
+static void SumProd(int i, int j, out int sum, out int prod)
+{
+    sum = i+j;
+    prod =i * j;
+}
