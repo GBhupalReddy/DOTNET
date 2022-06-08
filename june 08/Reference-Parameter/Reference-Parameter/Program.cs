91fc6fe6b8fb447ffe15c0c9967f21d8774c64b2
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-08 18:20:53 +0530

    Commit 08 june

---
+++ june 08/Reference-Parameter/Reference-Parameter/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 0;
+Console.WriteLine("Before using Reference Parameter {0} ", i);
+ReferenceParameter(ref i);
+Console.WriteLine($"After using Reference Parameter : {i}");
+static void ReferenceParameter(ref int j)
+{
+    j = 120;
+}
