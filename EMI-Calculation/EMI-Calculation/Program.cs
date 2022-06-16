ddc8eed3b7b99a1adc72bafa8af1af6e54c7166b
GBhupalReddy <bhupalg1998@gmail.com>
2022-06-16 10:57:54 +0530

    Commit EMI_Calculator

--- EMI-Calculation/EMI-Calculation/Program.cs
+++ EMI-Calculation/EMI-Calculation/Program.cs
@@ -2,8 +2,10 @@
 using EMI_Calculator;
 Console.WriteLine("Enter How Much of Amount You Want");
 long Amount=Convert.ToInt64(Console.ReadLine());
-Calculator.Calculation(Amount);
+Console.WriteLine("Enter How Much of You Pay DownPayment Amount");
+long Downpayment=Convert.ToInt64(Console.ReadLine());
+Calculator.Calculation(Amount,Downpayment);
 Console.WriteLine("Enter How many Months of EMI");
 int Months=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"You Pay Every Month Amount is : {Calculator.Emi(Amount,Months)}  ");
+Console.WriteLine($"You Pay Every Month Amount is : {Calculator.Emi(Amount - Downpayment, Months)}  ");
 
--- EMI-Calculation/EMI-Calculator/Calculator.cs
+++ EMI-Calculation/EMI-Calculator/Calculator.cs
@@ -2,21 +2,25 @@
 {
     public class Calculator
     {
-        public static void Calculation(long Amount)
+        public static void Calculation(long Amount, long Downpayment)
         {
-            float Percentage = (Amount / 100) * 5;
-            Console.WriteLine($"1  month You Pay Amount is  : {(Amount + (Percentage * 1))/1}");
-            Console.WriteLine($"2  month You Pay Amount is  : {(Amount + (Percentage * 2)) / 2}");
-            Console.WriteLine($"3  month You Pay Amount is  : {(Amount + (Percentage * 3)) / 3}");
-            Console.WriteLine($"4  month You Pay Amount is  : {(Amount + (Percentage * 4)) / 4}");
-            Console.WriteLine($"5  month You Pay Amount is  : {(Amount + (Percentage * 5)) / 5}");
-            Console.WriteLine($"6  month You Pay Amount is  : {(Amount + (Percentage * 6)) / 6}");
-            Console.WriteLine($"7  month You Pay Amount is  : {(Amount + (Percentage * 7)) / 7}");
-            Console.WriteLine($"8  month You Pay Amount is  : {(Amount + (Percentage * 8)) / 8}");
-            Console.WriteLine($"9  month You Pay Amount is  : {(Amount + (Percentage * 9)) / 9}");
-            Console.WriteLine($"10 month You Pay Amount is  : {(Amount + (Percentage * 10)) / 10}");
-            Console.WriteLine($"11 month You Pay Amount is  : {(Amount + (Percentage * 11)) / 11}");
-            Console.WriteLine($"12 month You Pay Amount is  : {(Amount + (Percentage * 12)) / 12}");
+            long RemainingAmount = Amount - Downpayment;
+            float Percentage = (RemainingAmount / 100) * 5;
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{i}  month You Pay Amount is  : {(RemainingAmount + (Percentage * i)) / i}");
+            }
+            //Console.WriteLine($"2  month You Pay Amount is  : {(ReminingAmount + (Percentage * 2)) / 2}");
+            //Console.WriteLine($"3  month You Pay Amount is  : {(Amount + (Percentage * 3)) / 3}");
+            //Console.WriteLine($"4  month You Pay Amount is  : {(Amount + (Percentage * 4)) / 4}");
+            //Console.WriteLine($"5  month You Pay Amount is  : {(Amount + (Percentage * 5)) / 5}");
+            //Console.WriteLine($"6  month You Pay Amount is  : {(Amount + (Percentage * 6)) / 6}");
+            //Console.WriteLine($"7  month You Pay Amount is  : {(Amount + (Percentage * 7)) / 7}");
+            //Console.WriteLine($"8  month You Pay Amount is  : {(Amount + (Percentage * 8)) / 8}");
+            //Console.WriteLine($"9  month You Pay Amount is  : {(Amount + (Percentage * 9)) / 9}");
+            //Console.WriteLine($"10 month You Pay Amount is  : {(Amount + (Percentage * 10)) / 10}");
+            //Console.WriteLine($"11 month You Pay Amount is  : {(Amount + (Percentage * 11)) / 11}");
+            //Console.WriteLine($"12 month You Pay Amount is  : {(Amount + (Percentage * 12)) / 12}");
         }
         public static float Emi(long Amount, int Months)
         {
