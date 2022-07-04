6be07ad644e6ab0f0a32df7b277fd05b908f6b76
GBhupalReddy <bhupalg1998@gmail.com>
2022-07-04 11:53:35 +0530

    Commit Generic practice program

---
+++ Generic-Demo/Generic-Demo.Core/Comparision.cs
@@ -0,0 +1,16 @@
+﻿namespace Generic_Demo.Core
+{
+    public class Comparision
+    {
+        public bool Compar<t>(t value1, t value2)
+        {
+            if(value1 == null || value2 == null)
+            {
+                return false;
+            }
+            return value1.Equals(value2);
+        }
+
+
+    }
+}
---
+++ Generic-Demo/Generic-Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Generic_Demo.Core;
+using System.Collections;
+using System.Diagnostics;
+
+Comparision cm = new Comparision();
+Console.WriteLine(cm.Compar<int>(3,5));
+Console.WriteLine(cm.Compar<string>("",""));
+var nonGenericList = new ArrayList { 1, 2, 3, 4, 5 };
+Stopwatch nonstopwatch= Stopwatch.StartNew();
+nonstopwatch.Start();
+nonGenericList.Sort();
+nonstopwatch.Stop();
+Console.WriteLine(nonstopwatch.Elapsed.TotalMilliseconds);
+var GenericList =new List<int> { 1, 2, 3, 4, 5 };
+Stopwatch GenericStopwatch = Stopwatch.StartNew();
+GenericStopwatch.Start();
+GenericList.Sort();
+GenericStopwatch.Stop();
+Console.WriteLine(GenericStopwatch.Elapsed.TotalMilliseconds);
